20f66450ed71e55595d9a39c4410c2a31be9e83e
Arun179 <arunchoudhary.akr@gmail.com>
2019-06-14 23:58:54 +0530

    Updates

---
+++ precache-manifest.4661e6474c83f692bb189c80a2c7de7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b07f13c5b67bdac8ed23548426cd5d",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/React/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "dbb0e5c24609c5b9dcc0",
+    "url": "/React/static/css/main.957768cd.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/React/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "dbb0e5c24609c5b9dcc0",
+    "url": "/React/static/js/main.bc75264b.chunk.js"
+  },
+  {
+    "revision": "283b01c4b2c734b504b3",
+    "url": "/React/static/js/runtime~main.d53f88d2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React/static/media/SEGA.12a3db8b.woff"
+  }
+]);
